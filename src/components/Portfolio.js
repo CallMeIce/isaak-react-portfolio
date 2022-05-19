412aab1858feebd41f1b5c52dc67dbd61820fed8
Isaak Morales <ice.isaak97@gmail.com>
2022-05-18 23:30:11 -0400

    working on projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,11 +8,30 @@ import Typography from '@mui/material/Typography';
 
 export default function Projects() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card className='cardStyle' sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
         height="140"
         image="../src/images/Text-editor.png"
+        alt="Text Editor Project"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Text Editor
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          This is a text editor where a user can write down notes and even
+          download the application for offline use!!
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button href='https://github.com/CallMeIce/Text-Editor' size="small">Github Repo</Button>
+        <Button href='https://secure-cliffs-39156.herokuapp.com/' size="small">Deployed Link</Button>
+      </CardActions>
+      <CardMedia
+        component="img"
+        height="140"
+        image={require('../images/Text-editor.png')}
         alt="Text Editor Project"
       />
       <CardContent>
@@ -50,34 +69,15 @@ export default function Projects() {
       <CardMedia
         component="img"
         height="140"
-        image="../src/images/Text-editor.png"
-        alt="Text Editor Project"
+        image="../src/images/Daily-Plan.png"
+        alt="Daily Planner Project"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Text Editor
+          Daily Planner
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          This is a text editor where a user can write down notes and even
-          download the application for offline use!!
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Github Repo</Button>
-        <Button size="small">Deployed Link</Button>
-      </CardActions>
-      <CardMedia
-        component="img"
-        height="140"
-        image="../src/images/Text-editor.png"
-        alt="Text Editor Project"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          WatchMe Project
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          This was the first team project
+          This is a daily planner which enables you to write any notes or appointments on that day with the current time highlighted from 9-5.
         </Typography>
       </CardContent>
       <CardActions>
@@ -89,3 +89,18 @@ export default function Projects() {
     
   );
 }
+
+// const styles = {
+//     media: {
+//       height: 0,
+//       paddingTop: '56.25%',
+//       marginTop:'30'
+//     }
+// };
+// <Card sx={{ maxWidth: 345 }}>
+// <CardMedia
+//   className='classes.media'
+//   image="../src/images/Text-editor.png"
+//   Title="Text Editor Project"
+//   style={styles.media}
+// />
