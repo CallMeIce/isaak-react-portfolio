5e28fc731d8622171b38b715dd3bc148e803dcf1
Isaak Morales <ice.isaak97@gmail.com>
2022-05-20 00:08:51 -0400

    Added text on my email page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Resume() {
   return (
-    <div> <h1>Welcome! To my Resume</h1></div>
+    <div> 
+      <h1>Welcome to my Resume Page</h1>
+        <h4>Down below you will find a link to download my Portfolio</h4>
+    </div>
   )
 }
