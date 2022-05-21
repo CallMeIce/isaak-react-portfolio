fbbded6407ba06960d8113e4d64024559706b2e5
Isaak Morales <ice.isaak97@gmail.com>
2022-05-21 03:02:03 -0400

    Added my pic and added a little about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import Me from '../images/picture-of-me.JPG'
+
+const styles = {
+  aboutStyle: {
+    justifyContent: 'center',
+    color: 'white',
+    textAlign: 'center'
+  },
+  imageStyle: {
+    width: '19%',
+    borderRadius: 40,
+  }
+
+
+
+};
+
 
 export default function About() {
   return (
-    <div> <h1>Welcome! To my About</h1></div>
+    <div style={styles.aboutStyle}> 
+    <p>Hello, I'm Isaak Morales a current student in the UPenn Bootcamp for Full Stack Web Development.</p>
+    <p>I am a hard worker whether it is working independently or with other people in a group.</p>
+    <p>Some technical skills I have acquired are: HTML, CSS, Nodejs, Reactjs, Express, Javascrip and Git.</p>
+    <p>I would like to learn more and create more projects to become even more fluent in them.</p>
+    <img style={styles.imageStyle} src={Me} alt='Me' />
+    </div>
   )
 }
