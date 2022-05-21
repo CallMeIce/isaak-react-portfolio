c602cd9fc0a5556152f437123c852610b2cfe993
Isaak Morales <ice.isaak97@gmail.com>
2022-05-21 04:12:32 -0400

    edited card and got a popover for framework used

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,113 +5,78 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Popover from '@mui/material/Popover';
+import './Portfolio.css'
 
 export default function Projects() {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handlePopoverOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+
     return (
-        <Card className='cardStyle' sx={{ maxWidth: 345 }}>
-            <div>
+        <div>
+            <Card className='One' sx={{ maxWidth: 345 }}>
                 <CardMedia
                     component="img"
                     height="140"
-                    image="../src/images/Text-editor.png"
-                    alt="Text Editor Project"
+                    image={require('../images/Daily-Plan.png')}
+                    alt="Daily Planner Project"
                 />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                        Text Editor
+                        Daily Planner
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        This is a text editor where a user can write down notes and even
-                        download the application for offline use!!
+                        This is a daily planner which enables you to write any notes or appointments on that day with the current time highlighted from 9-5.
                     </Typography>
                 </CardContent>
                 <CardActions>
-                    <Button href='https://github.com/CallMeIce/Text-Editor' size="small">Github Repo</Button>
-                    <Button href='https://secure-cliffs-39156.herokuapp.com/' size="small">Deployed Link</Button>
+                    <Button href='https://github.com/CallMeIce/Weekday-Planner' size="small">Github Repo</Button>
+                    <Button href='https://callmeice.github.io/Weekday-Planner/' size="small">Deployed Link</Button>
+                    <div>
+                        <Typography
+                            aria-owns={open ? 'mouse-over-popover' : undefined}
+                            aria-haspopup="true"
+                            onMouseEnter={handlePopoverOpen}
+                            onMouseLeave={handlePopoverClose}
+                        >
+                            Frameworks Used
+                        </Typography>
+                        <Popover
+                            id="mouse-over-popover"
+                            sx={{
+                                pointerEvents: 'none',
+                            }}
+                            open={open}
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            onClose={handlePopoverClose}
+                            disableRestoreFocus
+                        >
+                            <Typography sx={{ p: 1 }}>HTML,CSS,JQuery, and Moment.js</Typography>
+                        </Popover>
+                    </div>
                 </CardActions>
-            </div>
+            </Card>
 
-            <div className='cardStyle'>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image={require('../images/Text-editor.png')}
-                    alt="Text Editor Project"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Text Editor
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        This is a text editor where a user can write down notes and even
-                        download the application for offline use!!
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Github Repo</Button>
-                    <Button size="small">Deployed Link</Button>
-                </CardActions>
-            </div>
+            
 
-            <div className='cardStyle'>
-            <CardMedia
-                component="img"
-                height="140"
-                image="../src/images/Text-editor.png"
-                alt="Text Editor Project"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Text Editor
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    This is a text editor where a user can write down notes and even
-                    download the application for offline use!!
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Github Repo</Button>
-                <Button size="small">Deployed Link</Button>
-            </CardActions>
-            </div>
-
-            <div className='cardStyle'>
-            <CardMedia
-                component="img"
-                height="140"
-                image={require('../images/Daily-Plan.png')}
-                alt="Daily Planner Project"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Daily Planner
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    This is a daily planner which enables you to write any notes or appointments on that day with the current time highlighted from 9-5.
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Github Repo</Button>
-                <Button size="small">Deployed Link</Button>
-            </CardActions>
-            </div>
-
-        </Card>
-
+        </div>
     );
 }
-
-// const styles = {
-//     media: {
-//       height: 0,
-//       paddingTop: '56.25%',
-//       marginTop:'30'
-//     }
-// };
-// <Card sx={{ maxWidth: 345 }}>
-// <CardMedia
-//   className='classes.media'
-//   image="../src/images/Text-editor.png"
-//   Title="Text Editor Project"
-//   style={styles.media}
-// />
