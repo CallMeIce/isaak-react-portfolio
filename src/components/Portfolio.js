8bd5b45ca7d3fa593923440c8c4b388da3260713
Isaak Morales <ice.isaak97@gmail.com>
2022-05-21 04:30:54 -0400

    setting up second card with the function for it

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -22,8 +22,22 @@ export default function Projects() {
 
     const open = Boolean(anchorEl);
 
+    //*Second Card Popover 
+    const [anchorElTwo, setAnchorElTwo] = React.useState(null);
+
+    const handlePopoverOpenTwo = (event) => {
+        setAnchorElTwo(event.currentTarget);
+    };
+
+    const handlePopoverCloseTwo = () => {
+        setAnchorElTwo(null);
+    };
+
+    const openTwo = Boolean(anchorElTwo);
+
     return (
-        <div>
+        <div className='container'>
+
             <Card className='One' sx={{ maxWidth: 345 }}>
                 <CardMedia
                     component="img"
@@ -70,6 +84,57 @@ export default function Projects() {
                             disableRestoreFocus
                         >
                             <Typography sx={{ p: 1 }}>HTML,CSS,JQuery, and Moment.js</Typography>
+                        </Popover>
+                    </div>
+                </CardActions>
+            </Card>
+
+            <Card className='One' sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={require('../images/Text-editor.png')}
+                    alt="Text Editor"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Text Editor
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        This is a text editor that you are able to install and jot down some notes even if you're offline!
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button href='https://github.com/CallMeIce/Weekday-Planner' size="small">Github Repo</Button>
+                    <Button href='https://callmeice.github.io/Weekday-Planner/' size="small">Deployed Link</Button>
+                    <div>
+                        <Typography
+                            aria-owns={open ? 'mouse-over-popover' : undefined}
+                            aria-haspopup="true"
+                            onMouseEnter={handlePopoverOpenTwo}
+                            onMouseLeave={handlePopoverCloseTwo}
+                        >
+                            Frameworks Used
+                        </Typography>
+                        <Popover
+                            id="mouse-over-popover"
+                            sx={{
+                                pointerEvents: 'none',
+                            }}
+                            open={openTwo}
+                            anchorEl={anchorElTwo}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            onClose={handlePopoverCloseTwo}
+                            disableRestoreFocus
+                        >
+                            <Typography sx={{ p: 1 }}>Javascript, Express</Typography>
                         </Popover>
                     </div>
                 </CardActions>
