fbd1f55bcb1029e973c97f6171eaac8d69319aca
Isaak Morales <ice.isaak97@gmail.com>
2022-06-21 15:31:01 -0400

    Deleted resume pdf and updated the Resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
+const styles = {
+  resumeHeader: {
+    justifyContent: 'flex',
+    margin: '0px',
+    color: 'white',
+    textAlign: 'center'
+  },
+};
+
+
 export default function Resume() {
   return (
-    <div> 
+    <div style={styles.resumeHeader} > 
       <h1>Welcome to my Resume Page</h1>
-        <h4>Down below you will find a link to download my Portfolio</h4>
+        <h4>Here are some skills I have acquired</h4>
+          <p>HTML, CSS, Javascript, Jquery, Express, SQL, Handlebars, Nodejs, React</p>
+        <h3>If you'd like to  download my resume, on the bottom left of the footer will be a download icon. Click it and it will send you do my resume.</h3>
     </div>
   )
 }
